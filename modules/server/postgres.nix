9e4676c5e3f18735af8ea602e2c6373280dbfee2
Matthew Mangano <matthew.mangano@gmail.com>
2026-03-17 18:57:26 -0700

    fix: correct pgvector extension syntax for NixOS

--- modules/server/postgres.nix
+++ modules/server/postgres.nix
@@ -15,7 +15,7 @@
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_16;
-    extensions = ps: with ps; [pgvector];
+    extensions = with pkgs.postgresql_16.pkgs; [pgvector];
 
     # Only listen on localhost; Caddy/app connects via Unix socket or 127.0.0.1
     settings = {
